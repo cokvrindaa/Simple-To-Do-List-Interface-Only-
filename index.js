0b9303718d7c4473fb56beddf6d1641e24cbb5ab
master.cokorda@gmail.com <master.cokorda@gmail.com>
2024-06-08 17:14:02 +0800

    fix darkmode

--- index.js
+++ index.js
@@ -39,15 +39,17 @@ function tampil(done = false) {
     tomhapus.innerHTML = "Hapus";
     donebutton.innerHTML = "Done";
     edit.innerHTML = "Edit";
-
+    donetext.className = "mx-2.5 dark:text-white font-medium lg:mx-0 md:mx-0";
     tomhapus.className =
-      "tomhapus shadow-lg h-8 w-20 hover:bg-red-500 rounded-lg m-1";
+      "tomhapus shadow-lg h-8 w-20 sm:hover:bg-red-500 rounded-lg m-1 dark:border  dark:bg-white dark:shadow-md dark:shadow-slate-300";
     donebutton.className =
-      "done shadow-lg h-8 w-20 hover:bg-green-500 rounded-lg m-1";
+      "done shadow-lg h-8 w-20 sm:hover:bg-green-500 rounded-lg m-1 dark:border  dark:bg-white dark:shadow-md dark:shadow-slate-300";
+
     edit.className =
-      "edit shadow-lg h-8 w-20 hover:bg-yellow-500 rounded-lg m-1";
-    divarea.className = "divarea mx-2.5";
-    paragraf.className = "mx-1 mt-4";
+      "edit shadow-lg h-8 w-20 sm:hover:bg-yellow-500 rounded-lg m-1 dark:border  dark:bg-white dark:shadow-md dark:shadow-slate-300";
+
+    divarea.className = "divarea mx-1.5 lg:mx-0";
+    paragraf.className = "mx-1 mt-4 dark:text-white";
 
     divarea.append(paragraf);
     divarea.append(tomhapus);
@@ -58,7 +60,7 @@ function tampil(done = false) {
     if (done) {
       donebutton.setAttribute("done", true);
       donevalue += 1;
-      paragraf.className = "mx-1 mt-4 text-green-700 font-bold";
+      paragraf.className = "mx-1 mt-4 text-green-700 font-bold ";
     }
 
     donetext.innerHTML = "Done List: " + donevalue;
@@ -81,9 +83,9 @@ function tampil(done = false) {
       edittextbox.value = paragraf.innerText;
       okeditbutton.innerHTML = "OK";
       edittextbox.className =
-        "edittextbox shadow-md p-1 focus:ring-1 focus:border-sky-600 focus:outline-none focus:rounded-md placeholder:px-1 invalid:text-red-600 invalid:focus:ring-red-600 peer dark:shadow-slate-300 dark:shadow-lg";
+        "edittextbox shadow-md p-1  focus:ring-1 focus:border-sky-600 focus:outline-none focus:rounded-md placeholder:px-1 invalid:text-red-600 invalid:focus:ring-red-600 peer dark:shadow-slate-300 dark:shadow-md rounded-lg dark:text-black";
       okeditbutton.className =
-        "okeditbutton shadow-lg h-8 w-20 hover:bg-green-500 rounded-lg";
+        "okeditbutton shadow-lg h-8 w-20 hover:bg-green-500 rounded-lg dark:border  dark:bg-white dark:shadow-md dark:shadow-slate-300 dark:text-black mx-0.5 text-black font-normal";
 
       paragraf.innerHTML = "";
       paragraf.appendChild(edittextbox);
